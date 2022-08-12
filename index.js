534befc1acf6ff4f8d3d8607b0ed36ed4ef1e716
unknown <rajebul.alam17@gmail.com>
2022-08-12 22:30:42 +0600

    Data Load for UI

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -23,6 +23,13 @@ async function run() {
         // Get All Data
         app.get('/allDetails', async (req, res) => {
             const result = await detailsCollection.find().toArray();
+            res.send(result);
+        });
+
+        app.get('/details/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await detailsCollection.find(query).toArray();
             res.send(result);
         });
         
