d99a78ead99ced172340e79dc9e866dd18dcc12b
unknown <rajebul.alam17@gmail.com>
2022-08-12 16:44:02 +0600

    Server Site Initialized

---
+++ README.md
@@ -0,0 +1,1 @@
+# unihire-project-server-site
---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World to');
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p4sest4.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const detailsCollection = client.db("production").collection("details");
+
+        // Get All Data
+        app.get('/allDetails', async (req, res) => {
+            const result = await detailsCollection.find().toArray();
+            res.send(result);
+        });
+        
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+    console.log('Server site running', port);
+});
